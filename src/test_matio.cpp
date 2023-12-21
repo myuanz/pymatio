4725702a4ad9f3136e2981d2c12634d77339f5a8
myuanz <provefars@gmail.com>
2023-12-22 00:18:11 +0800

    more

---
+++ src/test_matio.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+extern "C" {
+    #include "matio.h"
+}
+
+// class Test: public mat_t {
+//     public:
+//         Test() {
+//             std::cout << "Test class created!" << std::endl;
+//         }
+//         ~Test() {
+//             std::cout << "Test class destroyed!" << std::endl;
+//         }
+// };
+
+
+int main() {
+    mat_t *matfp;
+    printf("size of matfp: %lu\n", sizeof(matfp));
+
+    matfp = Mat_CreateVer("test.mat", NULL, MAT_FT_MAT5);
+    if (matfp) {
+        std::cout << "Matio compilation test successful!" << std::endl;
+        Mat_Close(matfp);
+    } else {
+        std::cout << "Matio compilation test failed!" << std::endl;
+    }
+    
+    return 0;
+}
--- xmake.lua
+++ xmake.lua
@@ -1,23 +1,32 @@
 add_rules("mode.debug", "mode.release")
-set_languages("cxx11")
+-- set_languages("cxx11")
 
 add_requires("matio", {configs = {zlib = true, hdf5 = true, mat73 = true}})
-add_requires("pybind11", {configs = {python = "python"}})
+add_requires("pybind11")
 
-target("demo")
+target("pymatio")
     set_kind("shared")
     add_packages("matio", "pybind11")
     add_cxflags("$(shell python -m pybind11 --includes)")
 
-    -- add_files("src/*.cpp")
-    set_extension("$(shell python -c \"print%(__import__%('sysconfig'%).get_config_var%('EXT_SUFFIX'%)%)\")")
+    set_extension("$(shell python -c \"print%(__import__%('sysconfig'%).get_config_var%('EXT_SUFFIX'%), end=''%)\")")
 
-    add_files("src/main.cpp")
+    -- add_files("src/main.cpp")
+    add_files("src/*.cpp")
+    -- add_files("src/test_matio.cpp")
+
     add_includedirs("src")
 
-    after_build(
-        function(target)
-            local targetfile = target:targetfile()
-            os.cp(targetfile, path.join("./", path.filename(targetfile):sub(4)))
-        end
-    )
+    -- after_build(
+    --     function(target)
+    --         local targetfile = target:targetfile()
+    --         os.cp(targetfile, path.join("./", path.filename(targetfile):sub(4)))
+    --     end
+    -- )
+
+target("test-matio")
+    set_kind("binary")
+    add_packages("matio", "pybind11")
+
+    add_files("src/test_matio.cpp")
+    add_includedirs("src")

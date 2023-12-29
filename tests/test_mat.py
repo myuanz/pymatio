f6e2710dbdd6acad14ce5ae606d8f1216da1a5c5
myuanz <provefars@gmail.com>
2023-12-30 00:04:41 +0800

    simply test

--- tests/test_mat.py
+++ tests/test_mat.py
@@ -1,9 +1,5 @@
-import pytest
-import _pymatio as pm
+import pymatio as pm
 
 
 def test_version():
-    assert pm.get_library_version() == (1, 5, 24,)
-
-if __name__ == '__main__':
-    pytest.main()
+    assert pm.get_library_version() == (1, 5, 26)

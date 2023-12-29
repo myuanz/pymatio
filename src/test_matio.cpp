2778164dfcb5807f62f43b147433cea088845ca1
myuanz <provefars@gmail.com>
2023-12-29 20:12:06 +0800

    update

--- src/test_matio.cpp
+++ src/test_matio.cpp
@@ -1,22 +1,11 @@
 #include <iostream>
 
 #include "matio_private.h"
-// #include "matio.h"
-
-class Test: public mat_t {
-    public:
-        Test() {
-            std::cout << "Test class created!" << std::endl;
-        }
-        ~Test() {
-            std::cout << "Test class destroyed!" << std::endl;
-        }
-};
 
 
 int main() {
     mat_t *matfp;
-    printf("size of matfp: %lu\n", sizeof(Test));
+    printf("size of matfp: %lu\n", sizeof(mat_t));
 
     matfp = Mat_CreateVer("test.mat", NULL, MAT_FT_MAT5);
     if (matfp) {

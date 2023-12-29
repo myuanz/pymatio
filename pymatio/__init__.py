dd11e6b7bbb0de011a5a48fc6bcf35cb184b2438
myuanz <provefars@gmail.com>
2023-12-30 00:04:20 +0800

    load from any dll file

--- pymatio/__init__.py
+++ pymatio/__init__.py
@@ -1,1 +1,11 @@
-print('init')
+import importlib
+import importlib.util
+import sys
+from pathlib import Path
+
+spec = importlib.util.spec_from_file_location("libpymatio", Path(__file__).parent / "libpymatio.cp311-win_amd64.pyd")
+libpymatio = importlib.util.module_from_spec(spec)
+sys.modules["libpymatio"] = libpymatio
+spec.loader.exec_module(libpymatio)
+
+from libpymatio import get_library_version

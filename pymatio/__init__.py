4b1f83e61d50db6e171de52a30dc16ba7db45c82
myuanz <provefars@gmail.com>
2023-12-30 22:12:42 +0800

    load from dll

--- pymatio/__init__.py
+++ pymatio/__init__.py
@@ -2,10 +2,24 @@ import importlib
 import importlib.util
 import sys
 from pathlib import Path
+import sysconfig
 
-spec = importlib.util.spec_from_file_location("libpymatio", Path(__file__).parent / "libpymatio.cp311-win_amd64.pyd")
-libpymatio = importlib.util.module_from_spec(spec)
-sys.modules["libpymatio"] = libpymatio
-spec.loader.exec_module(libpymatio)
+dll_files = list(Path(__file__).parent.glob("*.pyd"))
+if not dll_files:
+    print('no dll files', flush=True)
+    import time
+    time.sleep(3600)
+target_dll = Path(__file__).with_name(f'libpymatio{sysconfig.get_config_var("EXT_SUFFIX")}')
+print(f'{dll_files=} {target_dll=}')
+try:
+
+    spec = importlib.util.spec_from_file_location("libpymatio", target_dll)
+    libpymatio = importlib.util.module_from_spec(spec)
+    sys.modules["libpymatio"] = libpymatio
+    spec.loader.exec_module(libpymatio)
+except Exception as e:
+    print(e, flush=True)
+    import time 
+    time.sleep(3600)
 
 from libpymatio import get_library_version

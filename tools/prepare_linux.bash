ea01c84dbff7696e536988362728f7be181c08ae
zhengmy <zhengmy@ion.ac.cn>
2024-09-30 23:31:32 +0800

    upg

--- tools/prepare_linux.bash
+++ tools/prepare_linux.bash
@@ -1,3 +1,5 @@
-curl -fsSL https://xmake.io/shget.text | bash
-echo 'alias python=python3' >> ~/.xmake/profile 
-source ~/.xmake/profile
+curl -fsSL https://xmake.io/shget.text | bash &&
+echo 'alias python=python3' >> ~/.xmake/profile &&
+echo 'source ~/.xmake/profile' >> ~/.bashrc &&
+export XMAKE_ROOT="y" &&
+echo $PATH

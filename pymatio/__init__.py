e816e7d991b73d92615ffecacb9cdf4ef381b191
zhengmy <zhengmy@ion.ac.cn>
2024-10-11 15:31:11 +0800

    use candidate_dlls

--- pymatio/__init__.py
+++ pymatio/__init__.py
@@ -6,17 +6,23 @@ import sysconfig
 
 EXT_SUFFIX = sysconfig.get_config_var("EXT_SUFFIX")
 
-target_dll = Path(__file__).parent / f'libpymatio{EXT_SUFFIX}'
-try:
-
-    spec = importlib.util.spec_from_file_location("libpymatio", target_dll)
-    libpymatio = importlib.util.module_from_spec(spec)
-    sys.modules["libpymatio"] = libpymatio
-    spec.loader.exec_module(libpymatio)
-except Exception as e:
-    import traceback
-    traceback.print_exc()
-    raise
+candidate_dlls = [
+    Path(__file__).parent / f'libpymatio{EXT_SUFFIX}',
+    Path(__file__).parent.parent / 'build' / f'libpymatio{EXT_SUFFIX}'
+]
+for target_dll in candidate_dlls:
+    if not target_dll.exists():
+        continue
+    try:
+        spec = importlib.util.spec_from_file_location("libpymatio", target_dll)
+        libpymatio = importlib.util.module_from_spec(spec)
+        sys.modules["libpymatio"] = libpymatio
+        spec.loader.exec_module(libpymatio)
+        break
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        raise
 
 # from libpymatio import get_library_version
 from libpymatio import *

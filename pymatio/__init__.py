a22a2ab420b4e0a4e18d6add424ea40c78ab41fa
provefars <provefars@gmail.com>
2025-07-07 22:06:45 +0800

    candidate dlls for new scikit-build-core

--- pymatio/__init__.py
+++ pymatio/__init__.py
@@ -6,13 +6,26 @@ import sysconfig
 
 EXT_SUFFIX = sysconfig.get_config_var("EXT_SUFFIX")
 plat_tag = sysconfig.get_platform().replace("-", "_").replace(".", "_")
+python_version = f"cp{sys.version_info.major}{sys.version_info.minor}"
+
+# Get all possible build directories
+build_root = Path(__file__).parent.parent / 'build'
+potential_build_dirs = []
+if build_root.exists():
+    potential_build_dirs = [d for d in build_root.iterdir() if d.is_dir()]
 
 raw_candidate_dlls = [
     Path(__file__).parent / f'libpymatio{EXT_SUFFIX}',
     Path(__file__).parent.parent / 'build' / f'libpymatio{EXT_SUFFIX}',
-    Path(__file__).parent.parent / 'build' / f'cp312-abi3-{plat_tag}' / 'Release' / f'libpymatio{EXT_SUFFIX}',
-    # *list(Path(__file__).parent.parent.glob(f'libpymatio{EXT_SUFFIX}'))
 ]
+
+# Add all build subdirectories to candidate paths
+for build_dir in potential_build_dirs:
+    raw_candidate_dlls.extend([
+        build_dir / f'libpymatio{EXT_SUFFIX}',
+        build_dir / 'Release' / f'libpymatio{EXT_SUFFIX}',
+    ])
+
 candidate_dlls = filter(lambda x: x.exists(), raw_candidate_dlls)
 candidate_dlls = sorted(candidate_dlls, key=lambda x: x.stat().st_size, reverse=True)
 
